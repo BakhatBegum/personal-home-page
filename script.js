8f36250306fe3746fb41769cbf2ad86c223193dc
Bakhat Begum <130062899+BakhatBegum@users.noreply.github.com>
2023-08-19 20:12:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('block').addEventListener('click', (event) => {
+  let element_url = document.getElementById('url');
+  element_url.innerText = 'https://github.com/BakhatBegum';
+  element_url.setAttribute("href", 'https://github.com/BakhatBegum');
+
+});
