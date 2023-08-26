0a5533bd8bb49baa9e87155f0853d5ca68e6ed20
Bakhat Begum <130062899+BakhatBegum@users.noreply.github.com>
2023-08-26 14:51:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ document.getElementById('count').addEventListener('click', (event) => {
   event.target.innerText = click_count;
 
 });
+
+function convertToNumber(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return 0
+  } else {
+    return number_value
+  }
+}
+
+document.getElementById('button').addEventListener('click', (event) => {
+  window.speechSynthesis.speak(new SpeechSynthesisUtterance(document.getElementById('text').value));
+
+});
